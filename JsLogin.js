5c7ae244a71f0deaa0105609b7c4719de4729c2a
cristofferalexander711 <60554172+cristofferalexander711@users.noreply.github.com>
2020-02-01 17:15:05 -0300

    Add files via upload

---
+++ JsLogin.js
@@ -0,0 +1,15 @@
+﻿function Login() {
+    var correo = document.getElementById("txtCorreo").value;
+    var password = document.getElementById("txtPassword").value;
+
+    if (correo === 'ejemplo@gmail.com' && password === '123456') {
+        window.location.replace("/home/HtmlHome1.html");
+    }    
+    else {
+        var element = document.getElementById("divError");
+        element.classList.remove("oculto");
+        element.classList.add("visible");
+    }
+   
+
+}
